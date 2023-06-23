868460ff326cc72ffb50b2df0e0eb1e654d3d4b2
ermatenklar <137293956+ermatenklar@users.noreply.github.com>
2023-06-23 07:55:27 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,1 @@
-function toggleText(event) {
-    var text = event.textContent || event.innerText;
-    if(text== "NEI") {
-        event.innerHTML = "JA"
-    } else {
-        event.innerHTML = "NEI"
-    }
-}
+
